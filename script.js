a51c8e49a560febf02dfe9bc559e56068ef5d87a
LuSntgo <lu.sntgo@gmail.com>
2021-11-10 01:12:09 -0300

    Test2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+let result = document.getElementById("result")
+
+
+
 //get axios
-axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((res)=>{})
+axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((res)=>{
+    
+    let imageURL = res.data.sprites.other["official-artwork"].front_default;
+    let image = document.createElement('img');
+
+    image.src = imageURL
+    result.appendChild(image)
+});
